e7a81f750ce95c8c6c9dd12ed25ec487f87bd0b0
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2023-06-20 11:43:44 -0400

    version 0.1.1

--- src/main/java/org/llin/twelvequotes/controller/QuotesController.java
+++ src/main/java/org/llin/twelvequotes/controller/QuotesController.java
@@ -1,12 +1,9 @@
 package org.llin.twelvequotes.controller;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.llin.twelvequotes.controller.util.JsonUtil;
 import org.llin.twelvequotes.model.AllQuotes;
-import org.llin.twelvequotes.model.SingleQuote;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +15,6 @@ public class QuotesController {
 	
 	private static final String QUOTES = "QUOTES";
 	private static final String SELECT_COUNTRY = "SELECT_COUNTRY";
-	private static final String SELECT_CURRENCY = "SELECT_CURRENCY";
-	private static final String SELECT_EXCHANGE = "SELECT_EXCHANGE";
-	private static final String SELECT_MIC_CODE = "SELECT_MIC_CODE";
 	private static final String SELECT_TYPE = "SELECT_TYPE";	
 	private static final String THYMELEAF_TEMPLATE = "quotes"; 
 	
@@ -34,9 +28,6 @@ public class QuotesController {
 			all_quotes.populateSets();
 			
 			modelAndView.addObject(SELECT_COUNTRY, all_quotes.getCountrySet());
-			modelAndView.addObject(SELECT_CURRENCY, all_quotes.getCurrencySet());
-			modelAndView.addObject(SELECT_EXCHANGE, all_quotes.getExchangeSet());
-			modelAndView.addObject(SELECT_MIC_CODE, all_quotes.getMic_codeSet());
 			modelAndView.addObject(SELECT_TYPE, all_quotes.getTypeSet());
 			modelAndView.addObject(QUOTES, all_quotes.getQuotes());
 		} catch (IOException e) {
--- src/main/java/org/llin/twelvequotes/model/AllQuotes.java
+++ src/main/java/org/llin/twelvequotes/model/AllQuotes.java
@@ -5,21 +5,21 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AllQuotes {
 	private List<SingleQuote> quotes;
 
 	private String selectedCountry = "United States";
-	
-	private Set<String>	currencySet;
-	private Set<String>	exchangeSet;
-	private Set<String>	mic_codeSet;
-	private Set<String>	countrySet;
-	private Set<String>	typeSet;
+
+	private Set<String> countrySet;
+	private Set<String> typeSet;
 
 	public AllQuotes(List<SingleQuote> quotes) {
 		this.quotes = quotes;
 	}
-		
+
 	public String getSelectedCountry() {
 		return selectedCountry;
 	}
@@ -27,21 +27,9 @@ public class AllQuotes {
 	public void setSelectedCountry(String selectedCountry) {
 		this.selectedCountry = selectedCountry;
 	}
-	
+
 	public List<SingleQuote> getQuotes() {
 		return quotes;
-	}
-
-	public Set<String> getCurrencySet() {
-		return currencySet;
-	}
-
-	public Set<String> getExchangeSet() {
-		return exchangeSet;
-	}
-
-	public Set<String> getMic_codeSet() {
-		return mic_codeSet;
 	}
 
 	public Set<String> getCountrySet() {
@@ -53,27 +41,18 @@ public class AllQuotes {
 	}
 
 	public void populateSets() {
-		List<String> lcur = new ArrayList<>();
-		List<String> lexc = new ArrayList<>();
-		List<String> lmic = new ArrayList<>();
 		List<String> lcty = new ArrayList<>();
 		List<String> ltyp = new ArrayList<>();
 
 		for (SingleQuote q : quotes) {
-			lcur.add(q.getCurrency());
-			lexc.add(q.getExchange());
-			lmic.add(q.getMic_code());
 			if (!q.getCountry().isEmpty()) {
 				lcty.add(q.getCountry());
 			}
-			ltyp.add(q.getType());			
+			ltyp.add(q.getType());
 		}
-		
-		currencySet = new TreeSet<>(lcur);
-		exchangeSet = new TreeSet<>(lexc);
-		mic_codeSet = new TreeSet<>(lmic);
-		countrySet  = new TreeSet<>(lcty);
-		typeSet     = new TreeSet<>(ltyp);
-	}	
- 
+
+		countrySet = new TreeSet<>(lcty);
+		typeSet = new TreeSet<>(ltyp);
+	}
+
 }
