e29b345dca29d754b990c2af04e6443b4b992e6e
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2023-07-26 23:37:53 -0400

    Add files via upload

--- src/main/java/org/llin/twelvequotes/controller/QuoteDetailRequestController.java
+++ src/main/java/org/llin/twelvequotes/controller/QuoteDetailRequestController.java
@@ -1,15 +1,19 @@
 package org.llin.twelvequotes.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.llin.twelvequotes.controller.util.ChunkerUtil;
 import org.llin.twelvequotes.model.AllQuotes;
+import org.llin.twelvequotes.model.SingleQuote;
 import org.llin.twelvequotes.model.SingleQuoteDetailRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -51,4 +55,33 @@ public class QuoteDetailRequestController extends Base {
 		return modelAndView;
 	}
 	
+	@PostMapping("/filter")
+	public ModelAndView filterQuotes(@RequestAttribute String name,
+									 @RequestAttribute String symbol,
+									 		      HttpSession session) {
+				
+		ModelAndView modelAndView = new ModelAndView("quotes");
+
+		AllQuotes all_quotes = (AllQuotes)session.getAttribute(ALL_QUOTES);
+
+		List<List<SingleQuote>> chunkedList = all_quotes.getChunkedList();
+		List<SingleQuote> list = new ArrayList<>();
+		
+		for (List<SingleQuote> l : chunkedList) {
+			for (SingleQuote s : l) {
+				if (s.getName().toUpperCase().startsWith(name.toUpperCase()) ||
+					s.getSymbol().toUpperCase().startsWith(symbol.toUpperCase())) {
+					list.add(s);
+				}
+			}	
+		}
+		
+		if (!list.isEmpty()) {
+			all_quotes.setChunkedList(ChunkerUtil.chunkList(list, LIST_INCREMENT));
+		}
+		modelAndView.addObject(ALL_QUOTES, all_quotes);
+		
+		return modelAndView;
+	}
+		
 }
