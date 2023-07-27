0b04a6d53a9ebd1cb7e174d393c3a3786262b601
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2023-07-27 19:52:37 -0400

    Add files via upload

--- src/main/java/org/llin/twelvequotes/controller/Base.java
+++ src/main/java/org/llin/twelvequotes/controller/Base.java
@@ -4,4 +4,5 @@ public class Base {
 	public static final String ALL_QUOTES = "ALL_QUOTES";
 	public static final String QUOTE_DETAIL_REQUEST = "QUOTE_DETAIL_REQUEST";
 	public static final String QUOTE_DETAIL_RESPONSE = "QUOTE_DETAIL_RESPONSE";
+	public static final String SEARCH_REQUEST = "SEARCH_REQUEST";
 }
--- src/main/java/org/llin/twelvequotes/controller/QuoteDetailRequestController.java
+++ src/main/java/org/llin/twelvequotes/controller/QuoteDetailRequestController.java
@@ -3,7 +3,9 @@ package org.llin.twelvequotes.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.llin.twelvequotes.controller.model.SearchRequest;
 import org.llin.twelvequotes.controller.util.ChunkerUtil;
+import org.llin.twelvequotes.controller.util.TabsUtil;
 import org.llin.twelvequotes.model.AllQuotes;
 import org.llin.twelvequotes.model.SingleQuote;
 import org.llin.twelvequotes.model.SingleQuoteDetailRequest;
@@ -11,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
@@ -21,23 +24,29 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/quotes")
-public class QuoteDetailRequestController extends Base {
+public class QuoteDetailRequestController<T extends SingleQuote> extends Base {
 	private static final Logger logger = LoggerFactory.getLogger(QuoteDetailRequestController.class);
 	
 	private int LIST_INCREMENT = 20;
 	
+	@SuppressWarnings("unchecked")
 	@GetMapping("/list")
 	public ModelAndView getAllQuotes(HttpSession session) {
-
-		ModelAndView modelAndView = new ModelAndView("quotes");
-		AllQuotes all_quotes = (AllQuotes)session.getAttribute(ALL_QUOTES);
 		
-		all_quotes.setChunkedList(ChunkerUtil.chunkList(all_quotes.getList(), LIST_INCREMENT));
+		ModelAndView modelAndView = new ModelAndView("quotes");
+		
+		AllQuotes<T> all_quotes = (AllQuotes<T>)session.getAttribute(ALL_QUOTES);
+		
+		ChunkerUtil.chunkList(all_quotes, LIST_INCREMENT);
+		TabsUtil.generateTabs(all_quotes);
+		
 		modelAndView.addObject(ALL_QUOTES, all_quotes);
+		modelAndView.addObject(SEARCH_REQUEST, new SearchRequest());
 		
 		return modelAndView;
 	}
 
+	@SuppressWarnings("unchecked")
 	@GetMapping("/symbol/{symbol}")
 	public ModelAndView getSingleQuoteBySymbol(@PathVariable String symbol, 
 			                                            HttpSession session) {
@@ -47,7 +56,7 @@ public class QuoteDetailRequestController extends Base {
 		
 		ModelAndView modelAndView = new ModelAndView("quoteDetailRequest");
 
-		AllQuotes all_quotes = (AllQuotes)session.getAttribute(ALL_QUOTES);
+		AllQuotes<T> all_quotes = (AllQuotes<T>)session.getAttribute(ALL_QUOTES);
 		all_quotes.setSelectedSymbol(symbol);
 		
 		modelAndView.addObject(ALL_QUOTES, all_quotes);
@@ -55,29 +64,41 @@ public class QuoteDetailRequestController extends Base {
 		return modelAndView;
 	}
 	
+	@SuppressWarnings("unchecked")
 	@PostMapping("/filter")
-	public ModelAndView filterQuotes(@RequestAttribute String name,
-									 @RequestAttribute String symbol,
-									 		      HttpSession session) {
+	public ModelAndView filterQuotes(@ModelAttribute(SEARCH_REQUEST) SearchRequest sReq,
+									                                   HttpSession session) {
 				
 		ModelAndView modelAndView = new ModelAndView("quotes");
 
-		AllQuotes all_quotes = (AllQuotes)session.getAttribute(ALL_QUOTES);
+		AllQuotes<T> all_quotes = (AllQuotes<T>)session.getAttribute(ALL_QUOTES);
 
 		List<List<SingleQuote>> chunkedList = all_quotes.getChunkedList();
 		List<SingleQuote> list = new ArrayList<>();
+		System.out.println(list.size());
+		System.out.println("sReq [" + sReq.getName() + "] [" + sReq.getSymbol() + "]");
 		
 		for (List<SingleQuote> l : chunkedList) {
 			for (SingleQuote s : l) {
-				if (s.getName().toUpperCase().startsWith(name.toUpperCase()) ||
-					s.getSymbol().toUpperCase().startsWith(symbol.toUpperCase())) {
-					list.add(s);
+				if (!sReq.getName().isBlank()) {
+					if (s.getName().toUpperCase().contains(sReq.getName().trim().toUpperCase())) {
+						list.add(s);
+					}
+				}
+				if (!sReq.getSymbol().isBlank()) {
+					if (s.getSymbol().toUpperCase().startsWith(sReq.getSymbol().trim().toUpperCase())) {
+						list.add(s);
+					}	
 				}
 			}	
 		}
 		
+		System.out.println(list.size());
+		
 		if (!list.isEmpty()) {
-			all_quotes.setChunkedList(ChunkerUtil.chunkList(list, LIST_INCREMENT));
+			all_quotes.setList(list);
+			ChunkerUtil.chunkList(all_quotes, LIST_INCREMENT);
+			TabsUtil.generateTabs(all_quotes);
 		}
 		modelAndView.addObject(ALL_QUOTES, all_quotes);
 		
