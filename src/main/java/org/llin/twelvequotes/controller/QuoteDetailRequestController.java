5d2d094842f6291bfb694ce744da12310016e5d3
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2023-08-04 20:44:18 -0400

    Add files via upload

--- src/main/java/org/llin/twelvequotes/controller/QuoteDetailRequestController.java
+++ src/main/java/org/llin/twelvequotes/controller/QuoteDetailRequestController.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 import org.llin.twelvequotes.controller.model.SearchRequest;
 import org.llin.twelvequotes.controller.util.ChunkerUtil;
-import org.llin.twelvequotes.controller.util.TabsUtil;
 import org.llin.twelvequotes.model.AllQuotes;
+import org.llin.twelvequotes.model.ChunkedList;
+import org.llin.twelvequotes.model.ChunkedSubList;
 import org.llin.twelvequotes.model.SingleQuote;
 import org.llin.twelvequotes.model.SingleQuoteDetailRequest;
 import org.slf4j.Logger;
@@ -16,7 +17,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,7 +28,7 @@ public class QuoteDetailRequestController<T extends SingleQuote> extends Base {
 	private static final Logger logger = LoggerFactory.getLogger(QuoteDetailRequestController.class);
 
 	public static final int QOUTES_PER_PAGE = 20;
-	public static final int TABS_PER_LINE = 9;
+	public static final int TABS_PER_LINE = 12;
 
 	@SuppressWarnings("unchecked")
 	@GetMapping("/list")
@@ -38,8 +38,7 @@ public class QuoteDetailRequestController<T extends SingleQuote> extends Base {
 
 		AllQuotes<T> all_quotes = (AllQuotes<T>) session.getAttribute(ALL_QUOTES);
 
-		ChunkerUtil.chunkList(all_quotes, QOUTES_PER_PAGE);
-		TabsUtil.generateTabs(all_quotes);
+		ChunkerUtil.chunkList(all_quotes, QOUTES_PER_PAGE, TABS_PER_LINE);
 
 		modelAndView.addObject(ALL_QUOTES, all_quotes);
 		modelAndView.addObject(SEARCH_REQUEST, new SearchRequest());
@@ -72,11 +71,11 @@ public class QuoteDetailRequestController<T extends SingleQuote> extends Base {
 
 		AllQuotes<T> all_quotes = (AllQuotes<T>) session.getAttribute(ALL_QUOTES);
 
-		List<List<SingleQuote>> chunkedList = all_quotes.getChunkedList();
+		ChunkedList<T> chunkedList = all_quotes.getChunkedList();
 		List<SingleQuote> list = new ArrayList<>();
 
-		for (List<SingleQuote> l : chunkedList) {
-			for (SingleQuote s : l) {
+		for (ChunkedSubList<T> l : chunkedList.getList()) {
+			for (SingleQuote s : l.getList()) {
 				if (!sReq.getName().isBlank()) {
 					if (s.getName().toUpperCase().contains(sReq.getName().trim().toUpperCase())) {
 						list.add(s);
@@ -92,8 +91,7 @@ public class QuoteDetailRequestController<T extends SingleQuote> extends Base {
 
 		if (!list.isEmpty()) {
 			all_quotes.setList(list);
-			ChunkerUtil.chunkList(all_quotes, QOUTES_PER_PAGE);
-			TabsUtil.generateTabs(all_quotes);
+			ChunkerUtil.chunkList(all_quotes, QOUTES_PER_PAGE, TABS_PER_LINE);
 		}
 		modelAndView.addObject(ALL_QUOTES, all_quotes);
 
