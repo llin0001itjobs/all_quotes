07cd24c6bf120095da503e789cd2cb8a696df78f
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2023-08-04 20:44:41 -0400

    Add files via upload

--- src/main/java/org/llin/twelvequotes/controller/util/ChunkerUtil.java
+++ src/main/java/org/llin/twelvequotes/controller/util/ChunkerUtil.java
@@ -5,19 +5,20 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.llin.twelvequotes.model.AllQuotes;
+import org.llin.twelvequotes.model.ChunkedList;
 import org.llin.twelvequotes.model.SingleQuote;
 
 public class ChunkerUtil<T extends SingleQuote> {
 
 	// Helper method to divide the list into smaller chunks	
-	@SuppressWarnings("unchecked")
-	public static <T> void chunkList(AllQuotes allQuotes, int chunkSize) {
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static <T> void chunkList(AllQuotes allQuotes, int chunkSize, int tabsPerLine) {
 		List<T> list = (List<T>) allQuotes.getList();
 		List<List<T>> chunkedList = IntStream.range(0, list.size()).boxed()
 				.collect(Collectors.groupingBy(index -> index / chunkSize)).values().stream()
 				.map(indices -> indices.stream().map(list::get).collect(Collectors.toList()))
 				.collect(Collectors.toList());
 			
-		allQuotes.setChunkedList(chunkedList);
+		allQuotes.setChunkedList(new ChunkedList(chunkedList, tabsPerLine));
 	}
 }
