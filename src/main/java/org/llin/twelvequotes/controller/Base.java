03a44e47a3d897972828e48abb03c7c8c0fcb362
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2023-12-22 14:45:17 -0500

    Delete src/main/java/org/llin/twelvequotes/controller/Base.java

--- src/main/java/org/llin/twelvequotes/controller/Base.java
+++
@@ -1,8 +0,0 @@
-package org.llin.twelvequotes.controller;
-
-public class Base {
-	public static final String ALL_QUOTES = "ALL_QUOTES";
-	public static final String QUOTE_DETAIL_REQUEST = "QUOTE_DETAIL_REQUEST";
-	public static final String QUOTE_DETAIL_RESPONSE = "QUOTE_DETAIL_RESPONSE";
-	public static final String SEARCH_REQUEST = "SEARCH_REQUEST";
-}
