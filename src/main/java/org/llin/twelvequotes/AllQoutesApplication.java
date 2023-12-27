b961d8d178f147809e5fc75c25ec5da2dbd544fa
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2023-12-26 21:20:12 -0500

    Add files via upload

--- src/main/java/org/llin/twelvequotes/AllQoutesApplication.java
+++ src/main/java/org/llin/twelvequotes/AllQoutesApplication.java
@@ -3,9 +3,11 @@ package org.llin.twelvequotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationProperties(prefix = "api")
+@EnableScheduling
 public class AllQoutesApplication {
 
 	public static void main(String[] args) {
